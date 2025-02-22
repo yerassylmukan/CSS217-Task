7afb9b3b11225f3681d8eea6f291988507b864b6
yerassylmukan <yerassyl.mukan@outlook.com>
2025-02-22 13:48:35 +0500

    Add swagger for auto documenting apis

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -2,8 +2,10 @@ using System.Text.Json.Serialization;
 using Identity;
 using Identity.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Persistence;
 using Persistence.Data;
+using WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,9 +16,48 @@ builder.Services.AddControllers()
     .AddJsonOptions(options =>
         options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+    c.EnableAnnotations();
+    c.SchemaFilter<CustomSchemaFilters>();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme.",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
+
+
 builder.Services.AddLogging();
 
 var app = builder.Build();
+
+app.Logger.LogInformation("Web API created...");
+
+app.Logger.LogInformation("Seeding Database...");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
