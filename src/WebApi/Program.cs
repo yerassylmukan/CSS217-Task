52d474c9ee09db2a5882a2e3c288f5fc14395202
yerassylmukan <yerassyl.mukan@outlook.com>
2025-02-22 13:45:08 +0500

    Add auto migration (ef)

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,11 +1,14 @@
 using System.Text.Json.Serialization;
 using Identity;
+using Identity.Data;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
+using Persistence.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddIdentityServiceCollection(builder.Configuration);
 builder.Services.AddPersistenceServiceCollection(builder.Configuration);
+builder.Services.AddIdentityServiceCollection(builder.Configuration);
 
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
@@ -15,4 +18,27 @@ builder.Services.AddLogging();
 
 var app = builder.Build();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var appDbContext = services.GetRequiredService<ApplicationDbContext>();
+    var appIdentityDbContext = services.GetRequiredService<AppIdentityDbContext>();
+    appDbContext.Database.Migrate();
+    appIdentityDbContext.Database.Migrate();
+}
+catch (Exception e)
+{
+    app.Logger.LogError(e, "An error occurred while seeding the database.");
+}
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+
 app.Run();
+
+public partial class Program
+{
+}
